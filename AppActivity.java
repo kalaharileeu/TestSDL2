d7ec508526c3e774678dc8597e6c59523b9256cc
cornel de lange <cfdelange@gmail.com>
2017-01-04 11:56:51 +1300

    This file goes in \src\com\test\game

---
+++ AppActivity.java
@@ -0,0 +1,22 @@
+package com.test.game;
+
+public class AppActivity extends org.libsdl.app.SDLActivity
+{
+//	static
+//	{
+//		System.loadLibrary( "NativeLib" );
+//	}
+
+//	public static AppActivity m_Activity;
+
+//	@Override protected void onCreate( Bundle icicle )
+//	{
+//		super.onCreate( icicle );
+
+//		m_Activity = this;
+
+//		onCreateNative( getApplication().getApplicationInfo().sourceDir );
+//	}
+
+//	public static native void onCreateNative( String APKName );
+};
